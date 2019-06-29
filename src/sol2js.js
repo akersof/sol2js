4eac37e627710cc7fca2f7fdc09f855755c3b550
akersof <akersof@gmail.com>
2019-06-29 23:00:49 +0200

    add Deployable contract type handling

--- src/sol2js.js
+++ src/sol2js.js
@@ -39,23 +39,34 @@ const _compile2json = async (fileName, absoluteOutDir) => {
         log.success(`compilation of ${fileName} succeed`);
     }catch(e) {
         log.error(e.stderr);
-        return;
+        throw new Error(e.stderr);
     }
 
 };
 
 exports.compile = async (fileName, outDir) => {
     outDir = (!outDir || outDir.length === 0) ? './' : outDir;
-    const absoluteOutDir = path.join(process.cwd(), outDir);
+    const absoluteOutDir = outDir;//path.join(process.cwd(), outDir);
     await _compile2json(fileName, absoluteOutDir);
-    const jsonBaseName = fileName.slice(0, -4) + '.json';
-    const jsBaseName = fileName.slice(0, -4) + '.js';
+    const jsonBaseName = path.basename(fileName).slice(0, -4) + '.json';
+    const jsBaseName = path.basename(fileName).slice(0, -4) + '.js';
 
     //rename json file
     await fs.rename(path.join(absoluteOutDir, JSON_FILENAME), path.join(absoluteOutDir, jsonBaseName));
     let obj = (await fs.readJson(path.join(absoluteOutDir, jsonBaseName))).contracts;
     const contracts = {};
-    Object.keys(obj).forEach((key, index) => {
+    Object.keys(obj)
+        //TODO: create a filter function
+        .filter((elem) => {
+            if (path.basename(elem.split(':')[0]) === "Deployable.sol" || elem.split(':')[1] === "Deployable") return false;
+            const abi = JSON.parse(obj[elem].abi);
+            console.log(abi);
+            for(let i = 0; i < abi.length; ++i) {
+                if(abi[i].name === "deployable") return true;
+            }
+            return false;
+        } )
+        .forEach((key, index) => {
         const clef = key.split(":").pop();
         const abi = JSON.parse(obj[key].abi);
         const bin = obj[key].bin;
@@ -76,7 +87,7 @@ class ${key} {
     }
     //let moduleExports = `modules.exports{${...keys}}`;
     const jsContent = `const contracts = ${JSON.stringify(contracts, null, 2)};\n`;
-    await fs.writeFile(path.join(absoluteOutDir, fileName.slice(0, -4) + '.js'), jsContent + classContent);
+    await fs.writeFile(path.join(absoluteOutDir, jsBaseName), jsContent + classContent);
 
     //const jsonPath = path.join(absoluteOutDir, JSON_FILENAME);
     //const files = await fs.readdir(absoluteOutDir);
@@ -91,6 +102,7 @@ class ${key} {
 const abi2Method = (abiList) => {
     let method = '';
     for(const func of abiList) {
+        if(func.type !== 'function') continue;
         const args = func.inputs.map((elem) => `${elem.type}_${elem.name}`);
         func.payable ? args.push('overrides') : '';
         method += `
